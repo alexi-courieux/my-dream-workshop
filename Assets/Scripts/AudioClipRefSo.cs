56b331b237e40b2ddfeb89f92a8a1bf20c114859
Alexi Courieux <courieuxa@gmail.com>
2024-10-05 15:30:42 -0400

    namespace deleted

--- Assets/Scripts/AudioClipRefSo.cs
+++ Assets/Scripts/AudioClipRefSo.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 
-namespace AshLight.BakerySim
+[CreateAssetMenu(fileName = "AudioClip_new", menuName = "ScriptableObject/AudioClipRef")]
+public class AudioClipRefSo : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "AudioClip_new", menuName = "ScriptableObject/AudioClipRef")]
-    public class AudioClipRefSo : ScriptableObject
-    {
-        public AudioClip[] audioClips;
+    public AudioClip[] audioClips;
 
-        public AudioClip GetRandomClip()
-        {
-            return audioClips[Random.Range(0, audioClips.Length)];
-        }
+    public AudioClip GetRandomClip()
+    {
+        return audioClips[Random.Range(0, audioClips.Length)];
     }
 }
