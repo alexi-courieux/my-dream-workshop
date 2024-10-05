998670713b38f79cf5c7a604c297b3548a060c0a
Alexi Courieux <courieuxa@gmail.com>
2024-10-05 16:25:28 -0400

    Blender station script

---
+++ Assets/Scripts/Utils/StackList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public class StackList<T> : List<T>
+    {
+        public void Push(T item)
+        {
+            Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = this[Count - 1];
+            RemoveAt(Count - 1);
+            return item;
+        }
+        
+        public T Shift()
+        {
+            T item = this[0];
+            RemoveAt(0);
+            return item;
+        }
+        
+        public T PeekLast()
+        {
+            return Count != 0  ?  this[Count - 1] : default;
+        }
+        
+        public T PeekFirst()
+        {
+            return Count != 0 ? this[0] : default;
+        }
+    }
+}
